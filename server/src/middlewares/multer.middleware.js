61bb2178c2bc286096b7acc09e89ca9a0153dfa2
Rohit-1717 <rohitvishwakarma1717@gmail.com>
2024-08-17 15:18:52 +0530

    Added gitIgnore and gitkeep

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -1,6 +1,9 @@
 import multer from "multer";
 import path from "path";
 
+const uploadDir = path.resolve(__dirname, '../../public/uploads');
+
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
